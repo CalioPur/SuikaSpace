60a23b608f81b624f5f7a149e03fd68938677250
VeryDeadMoth <nix.axius@gmail.com>
2024-06-06 18:39:44 +0200

    Edited Attract Planets

--- Assets/AttractPlanets.cs
+++ Assets/AttractPlanets.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))] //forces sun to have rigidbody
 public class AttractPlanets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private float gravity = 1;
+
+    // we could make a list of attractors, but right now, only the primary sun is one
+    private Rigidbody sun;
+    public static List<Rigidbody> planets = new(); // list of planets (attracted)
+
+    public bool isSimulating; //do we want to simulate gravity right now ?
+
+    private void Awake()
     {
-        print("helloWorld");
+        sun = GetComponent<Rigidbody>(); // fetch and cache sun's rigidbody 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (isSimulating)
+        {
+            SimulateGravity();
+        }   
+    }
+
+    public void SimulateGravity()
+    {
+        foreach (Rigidbody body in planets)
+        {
+            AddGravitationalForce(sun, body);
+        }
+    }
+
+    public void AddGravitationalForce(Rigidbody attractor, Rigidbody attracted)
+    {
+        float massProduct = attractor.mass * attracted.mass * gravity;
+
+        Vector3 difference = attractor.position - attracted.position;
+        float distance = Vector3.Distance(attractor.position, attracted.position);
+
+        // Force = gravity * (( mass 1 * mass 2 ) / distance^2)
+        float scaledForce = gravity * (massProduct / distance*distance);
+
+        Vector3 forceDirection = Vector3.Normalize(difference);
+        Vector3 forceVector = forceDirection * scaledForce;
+
+        attracted.AddForce(forceVector); //makes our target move closer to the attractor
+
     }
 }
